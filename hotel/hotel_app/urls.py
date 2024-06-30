ca6f01b0119e20825c52f20c11f63f585d9d699c
ilyailya8989 <ilya.umrudin@mail.ru>
2024-06-30 11:48:49 +0300

    Add files via upload
    отель с регистрацией

--- hotel/hotel_app/urls.py
+++ hotel/hotel_app/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from hotel_app import views
 
 urlpatterns = [
-    path('rooms/', views.first_list, name='first'),
+    path('', views.first_list, name='first'),
     path('rooms/<int:room_id>', views.info_list, name='second'),
     path('rooms/add_room/', views.add_room, name='add_room'),
-
-
-
+    path('rooms/del_room/<int:room_id>', views.del_room, name='del_room'),
+    path('rooms/edit_room/<int:room_id>', views.edit_room, name='edit_room'),
 ]
--- hotel/hotel_app/views.py
+++ hotel/hotel_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
 from hotel_app.models import Hotel, Room
 
 
-# Create your views here.
 def first_list(request):
     rooms = Room.objects.all()
     return render(request,'hotel_app/hotel_list.html',{'rooms':rooms})
@@ -11,6 +9,17 @@ def first_list(request):
 def info_list(request, room_id):
     rooms = get_object_or_404(Room, id=room_id)
     return render(request, 'hotel_app/info_room.html',{'rooms': rooms})
+
+def edit_room(request, room_id):
+    rooms = get_object_or_404(Room, id=room_id)
+    if request.method == 'POST':
+        rooms.classification =request.POST.get('classification')
+        rooms.priceOnNight =request.POST.get('priceOnNight')
+        rooms.description = request.POST.get('description')
+        rooms.save()
+        return redirect('first', room_id=rooms.id)
+    return render(request, 'hotel_app/edit_room.html', {'rooms': rooms})
+
 
 def add_room(request):
     hostel = get_object_or_404(Hotel, id=1)
@@ -23,8 +32,18 @@ def add_room(request):
         rooms = request.POST.get('rooms')
 
         if numbers and classification and priceOnNight and description and rooms:
-            Room.objects.create(numbers=numbers,classification=classification,priceOnNight=priceOnNight,description=description, rooms=rooms)
+            Room.objects.create(numbers=numbers,
+                                classification=classification,
+                                priceOnNight=priceOnNight,
+                                description=description,
+                                rooms=hostel)
 
             return redirect('first')
 
     return render(request, 'hotel_app/add_room.html', {'hostel':hostel})
+
+def del_room(request, room_id):
+    room = get_object_or_404(Room, id=room_id)
+    room.delete()
+    return render(request, 'hotel_app/hotel_list.html', {'rooms':room})
+
